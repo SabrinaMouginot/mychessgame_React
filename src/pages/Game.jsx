83ef8745a5f04540a5549a3110b5cfa5b0d3379b
SabrinaMouginot <s.mouginot@yahoo.fr>
2025-01-10 15:24:31 +0100

    insertion des pièces dans le Game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,32 @@
 const Game = () => {
-  return (
-    <div>
-      <p>A ton tour!</p>
-    </div>
-  );
+    const pieces = {
+        black: {
+            rook: '/src/assets/images/pieces/black/rook.png',
+            knight: '/src/assets/images/pieces/black/knight.png',
+            bishop: '/src/assets/images/pieces/black/bishop.png',
+            queen: '/src/assets/images/pieces/black/queen.png',
+            king: '/src/assets/images/pieces/black/king.png',
+            pawn: '/src/assets/images/pieces/black/pawn.png',
+        },
+        white: {
+            rook: '/src/assets/images/pieces/white/rook.png',
+            knight: '/src/assets/images/pieces/white/knight.png',
+            bishop: '/src/assets/images/pieces/white/bishop.png',
+            queen: '/src/assets/images/pieces/white/queen.png',
+            king: '/src/assets/images/pieces/white/king.png',
+            pawn: '/src/assets/images/pieces/white/pawn.png',
+        },
+    };
+
+    const renderPiece = () => (
+        <img src={pieces} alt="piece" />
+    )
+
+    return (
+        <div className="chess-board">
+            <div className="cell">{renderPiece()}</div>
+        </div>
+    );
 };
 
 export default Game;
